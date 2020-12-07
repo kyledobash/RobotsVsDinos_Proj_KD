7328ca9f149692b127aef2c812f505ad7742479e
kyledobash <kyledobash@yahoo.com>
2020-12-07 09:38:14 -0700

    Created all classes and built out class skeletons for each, creating member variables and initializing in the constructor

---
+++ RobotsVsDinos/Robot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinos
+{
+    public class Robot
+    {
+        //MEMBER VARIABLES (what it has)
+        string name;
+        int health;
+        int powerLevel;
+        Weapon weapon;
+        int attackPower;
+
+        //CONSTRUCTOR (spawner)
+
+        //MEMBER METHODS (what can it do)
+    }
+}
